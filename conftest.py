2d84befd4468e53fcbc79d98b7ea3e86e6467f22
Sergei Rybin <sergeyrybin88@gmail.com>
2024-02-06 20:30:37 +0300

    Добавил персональные данные и тест на авторизацию и параметризацию

--- conftest.py
+++ conftest.py
@@ -2,11 +2,12 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
+import time
 
 @pytest.fixture(scope="session")
 def load_person_date():
     # Открываем файл с конфигом в режиме чтения
-    with open('config.json', 'r') as config_file:
+    with open('person.json', 'r') as config_file:
         # С помощью библиотеки json читаем и возвращаем результат
         config = json.load(config_file)
         return config
@@ -15,6 +16,8 @@ def load_person_date():
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
+
--- test_param_stepik.py
+++ test_param_stepik.py
@@ -1,9 +1,28 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 class TestLogin:
-    def test_authorization(self, browser, wait, load_config):
+    def test_authorization(self, browser, load_person_date):
 
-        login = load_config['login_stepik']
-        password = load_config['password_stepik']
+        login = load_person_date['login_stepik']
+        password = load_person_date['password_stepik']
+        link = 'https://stepik.org/lesson/236895/step/1'
+
+        browser.get(link)                # Переходим по ссылке
+
+        login_btn = browser.find_element(By.XPATH, '//a[@href="/lesson/236895/step/1?auth=login"]')
+        login_btn.click()         # Нажимаем кнопку ВОЙТИ
+
+        login_field = browser.find_element(By.XPATH, '//input[@name="login"]')
+        login_field.send_keys(login) # Вводим логин
+
+        password_field = browser.find_element(By.XPATH, '//input[@name="password"]')
+        password_field.send_keys(password) # Вводим пароль
+
+        btn = browser.find_element(By.XPATH, '//button[@type="submit"]')
+        btn.click()
+
+        time.sleep(5)
+
