14c70d3f68468560c05eb5b71884205fbcc20663
Sergei Rybin <sergeyrybin88@gmail.com>
2024-02-07 14:06:33 +0300

    Доработал тест на параметризацию

--- test_param_stepik.py
+++ test_param_stepik.py
@@ -2,27 +2,47 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import math
 
-class TestLogin:
-    def test_authorization(self, browser, load_person_date):
 
-        login = load_person_date['login_stepik']
-        password = load_person_date['password_stepik']
-        link = 'https://stepik.org/lesson/236895/step/1'
+@pytest.mark.parametrize('link', ['https://stepik.org/lesson/236895/step/1',
+                                'https://stepik.org/lesson/236896/step/1',
+                                'https://stepik.org/lesson/236897/step/1',
+                                'https://stepik.org/lesson/236898/step/1',
+                                'https://stepik.org/lesson/236899/step/1',
+                                'https://stepik.org/lesson/236903/step/1]',
+                                'https://stepik.org/lesson/236904/step/1',
+                                'https://stepik.org/lesson/236905/step/1'])
+def test_authorization(browser, load_person_date, link):
 
-        browser.get(link)                # Переходим по ссылке
+    login = load_person_date['login_stepik']
+    password = load_person_date['password_stepik']
+    answer = math.log(int(time.time()))
 
-        login_btn = browser.find_element(By.XPATH, '//a[@href="/lesson/236895/step/1?auth=login"]')
-        login_btn.click()         # Нажимаем кнопку ВОЙТИ
+    browser.get(link)                # Переходим по ссылке
 
-        login_field = browser.find_element(By.XPATH, '//input[@name="login"]')
-        login_field.send_keys(login) # Вводим логин
+    login_btn = browser.find_element(By.LINK_TEXT, 'Войти')
+    login_btn.click()         # Нажимаем кнопку ВОЙТИ
 
-        password_field = browser.find_element(By.XPATH, '//input[@name="password"]')
-        password_field.send_keys(password) # Вводим пароль
+    login_field = browser.find_element(By.XPATH, '//input[@name="login"]')
+    login_field.send_keys(login) # Вводим логин
 
-        btn = browser.find_element(By.XPATH, '//button[@type="submit"]')
-        btn.click()
+    password_field = browser.find_element(By.XPATH, '//input[@name="password"]')
+    password_field.send_keys(password) # Вводим пароль
 
-        time.sleep(5)
+
+    btn = browser.find_element(By.XPATH, '//button[@type="submit"]')
+    btn.click()
+
+    input_txt = browser.find_element(By.CSS_SELECTOR, 'div > textarea').send_keys(str(answer))
+
+    send_answer = browser.find_element(By.CLASS_NAME, 'submit-submission')
+    send_answer.click()
+
+    control_field = browser.find_element(By.CSS_SELECTOR, 'p .smart-hints__hint').text
+
+    print(control_field)
+
+    assert control_field == 'Correct!', 'Ошибка! Текст не соответсвует слову Correct!'
+
 
